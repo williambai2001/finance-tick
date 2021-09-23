2b30b9edd66b071e31f1e576809ad3504b077d51
william <william@williamdeMacBook-Pro.local>
2021-09-23 16:19:36 +0800

    update testing.

--- test/services/tick.service.test.js
+++ test/services/tick.service.test.js
@@ -1,20 +1,36 @@
 const _ = require('underscore');
 const moment = require('moment');
-const TickService = require('../../src/services/tick.service');
+const {ServiceBroker} = require('moleculer');
+const TickServiceSchema = require('../../src/services/tick.service');
 
-describe('tick.service.',()=>{
+describe('tick.service',()=>{
+	let broker = new ServiceBroker({logger: false});
+	let tickService = broker.createService(TickServiceSchema);
+	jest.setTimeout(30000);
+	beforeAll(()=>broker.start());
+	afterAll(()=>broker.stop());
 
-	it('.requestTick()',async ()=>{
-		expect(TickService).toHaveProperty('methods');
-		let methods = TickService.methods;
-		expect(methods).toHaveProperty('requestTick');
-		let symbols = ['sh000001'];
-		let tick = await methods.requestTick(symbols);
-		expect(tick).toBeInstanceOf(Object);
-		expect(tick).toHaveProperty(symbols[0]);
-		expect(tick[symbols[0]]).toHaveProperty('symbol');
-		expect(tick[symbols[0]]).toHaveProperty('name');
-		expect(tick[symbols[0]]).toHaveProperty('date');
-		expect(tick[symbols[0]]).toHaveProperty('time');
+	describe('.actions',()=>{
+		it('.get()',async ()=>{
+			const tick = await broker.call('Tick.get');
+			console.log(`tick: ${tick}`);
+			expect(tick).toBeInstanceOf(Object);
+		});
+		it('.getAll()',async ()=>{
+
+		});
+	});
+	xdescribe('.methods',()=>{
+		it('.requestTick()',async ()=>{
+			let symbols = ['sh000001'];
+			let tick = await tickService.requestTick(symbols).catch(console.error);
+			// console.log(tick);
+			expect(tick).toBeInstanceOf(Object);
+			expect(tick).toHaveProperty(symbols[0]);
+			expect(tick[symbols[0]]).toHaveProperty('symbol');
+			expect(tick[symbols[0]]).toHaveProperty('name');
+			expect(tick[symbols[0]]).toHaveProperty('date');
+			expect(tick[symbols[0]]).toHaveProperty('time');
+		});
 	});
 });
